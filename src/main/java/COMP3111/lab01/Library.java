007998c7a34e2c8e44a0c5f61e6ee383d884389a
YYYeung <sheepyy039@gmail.com>
2023-02-15 19:48:31 +0800

    Update main in Library.java

--- src/main/java/COMP3111/lab01/Library.java
+++ src/main/java/COMP3111/lab01/Library.java
@@ -5,7 +5,9 @@ package COMP3111.lab01;
 
 public class Library {
 	public static void main(String arg[] ){
-		System.out.println("Hello, World!");
+		Library lib = new Library();
+		if (lib.someLibraryMethod())
+			System.out.println("Hello, World!");
 	}
     public boolean someLibraryMethod() {
         return true;
